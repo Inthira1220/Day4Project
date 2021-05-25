c30e0d918b2a653801cb132fe233b8ddd999e748
Inthira K <kefind@gmail.com>
2021-05-25 19:24:04 -0400

    share on github

--- src/test/java/BranchPractice.java
+++ src/test/java/BranchPractice.java
@@ -1,5 +1,11 @@
 public class BranchPractice {
     public static void main(String[] args) {
 
+        // create the new project
+        //enable version control
+        //added .gitinore with idea, target folder and iml file
+        // made initial commit by keeping track of all files
+        //share
+
     }
 }
